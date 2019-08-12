ee26af3f290bbfcda4aa8e237214ce93a322c6fb
= <karan_sachdev_2@sfu.ca>
2019-08-11 23:19:16 -0700

    implemnted function that will fetch user's dapper address.

--- cheese-wizards-webapp/src/components/Hello.vue
+++ cheese-wizards-webapp/src/components/Hello.vue
@@ -16,32 +16,49 @@ export default {
     }
   },
   created: function () {
-    debug()
+    // debug()
+    init()
   }
 }
 
-const Web3 = require('web3')
-const DappAuth = require('@dapperlabs/dappauth')
-
-const dappAuth = new DappAuth(new Web3.providers.HttpProvider('http://localhost:8545'))
-
-async function debug () {
-  const challenge = 'foo'
-  const signature = '0x33838c6f4e621982c2009f9b93ecb854a4b122538159623abc87d2e4c5bd6d2e33591f443b419b3bd2790e455ba6d625f2ca14b822c5cef824ef7e9021443bed1c'
-  const address = '0x86aa354fc865925f945b803ceae0b3f9d856b269'
+async function init () {
+  if (typeof window.ethereum === 'undefined') {
+    // Handle case where user hasn't installed Dapper.
+    return
+  }
 
   try {
-    const isAuthorizedSigner = await dappAuth.isAuthorizedSigner(
-      challenge,
-      signature,
-      address
-    )
-
-    console.log(isAuthorizedSigner) // true
-  } catch (e) {
-    console.log(e)
+    // If a user is logged in to Dapper and has previously approved the dapp,
+    // `ethereum.enable` will return the result of `eth_accounts`.
+    const accounts = await window.ethereum.enable()
+    console.log(accounts)
+  } catch (error) {
+    // Handle error. If the user rejects the request for access, then
   }
 }
+
+// const Web3 = require('web3')
+// const DappAuth = require('@dapperlabs/dappauth')
+
+// const dappAuth = new DappAuth(new Web3.providers.HttpProvider('http://localhost:8080'))
+
+// async function debug () {
+//   const challenge = 'foo'
+//   const signature = '0x33838c6f4e621982c2009f9b93ecb854a4b122538159623abc87d2e4c5bd6d2e33591f443b419b3bd2790e455ba6d625f2ca14b822c5cef824ef7e9021443bed1c'
+//   const address = '0x86aa354fc865925f945b803ceae0b3f9d856b269'
+
+//   try {
+//     const isAuthorizedSigner = await dappAuth.isAuthorizedSigner(
+//       challenge,
+//       signature,
+//       address
+//     )
+
+//     console.log(isAuthorizedSigner) // true
+//   } catch (e) {
+//     console.log(e)
+//   }
+// }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
